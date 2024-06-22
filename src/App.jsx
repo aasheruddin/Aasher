e9ba4017cd08543884753808dc832992693f72d8
aasheru <aasheruddin123@gmail.com>
2024-06-22 14:26:02 -0600

    update using react

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import About from '../components/About';
+
+
+function App() {
+    return (
+        <div className="App">
+           
+            <main>
+               
+                <About />
+                
+                
+            </main>
+           
+        </div>
+    );
+}
+
+export default App;
